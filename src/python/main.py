0fd765fce9d16ddfe2784cbac1a1d3002945d40b
Gad Marconi <gadmarconi4@outlook.com>
2024-05-14 14:48:17 +0200

    Refactor RNN forward method for improved readability and performance

--- src/python/main.py
+++ src/python/main.py
@@ -1,8 +1,8 @@
 from networks.rnn.tests.test_rnn import test_rnn, test_rnn_on_sine_wave, test_rnn_with_adagrad, test_rnn_with_adam, test_rnn_with_grad_descent
 
 if __name__ == '__main__':
-    # test_rnn()
-    # test_rnn_on_sine_wave()
+    test_rnn()
+    test_rnn_on_sine_wave()
     test_rnn_with_adagrad()
     test_rnn_with_adam()
     test_rnn_with_grad_descent()
--- src/python/utils/layers.py
+++ src/python/utils/layers.py
@@ -7,41 +7,68 @@ class Layer:
         self.output_size = output_size
         self.weights = np.random.randn(output_size, input_size) * np.sqrt(2. / input_size)
         self.biases = np.zeros((output_size, 1))
-        self.activation = activation
+        
+        # Predefine activation and gradient functions
+        if activation == 'relu':
+            self.activation = self.relu
+            self.activation_grad = self.relu_grad
+        elif activation == 'sigmoid':
+            self.activation = self.sigmoid
+            self.activation_grad = self.sigmoid_grad
+        elif activation == 'tanh':
+            self.activation = self.tanh
+            self.activation_grad = self.tanh_grad
+        else:
+            self.activation = self.linear
+            self.activation_grad = self.linear_grad
 
     def forward(self, x):
         if x.ndim == 1:
             x = x.reshape(-1, 1)
         self.input = x
         self.z = np.dot(self.weights, x) + self.biases
-        return self.apply_activation(self.z)
-
-    def apply_activation(self, z):
-        if self.activation == 'relu':
-            return np.maximum(0, z)
-        elif self.activation == 'sigmoid':
-            return 1 / (1 + np.exp(-z))
-        elif self.activation == 'tanh':
-            return np.tanh(z)
-        else:
-            return z
+        return self.activation(self.z)
 
     def backward(self, grad_output):
         if grad_output.ndim == 1:
             grad_output = grad_output.reshape(-1, 1)
-        if self.activation == 'relu':
-            grad_z = grad_output * (self.z > 0).astype(float)
-        elif self.activation == 'sigmoid':
-            sig = self.apply_activation(self.z)
-            grad_z = grad_output * sig * (1 - sig)
-        elif self.activation == 'tanh':
-            tanh = self.apply_activation(self.z)
-            grad_z = grad_output * (1 - tanh ** 2)
-        else:
-            grad_z = grad_output
-
+        grad_z = grad_output * self.activation_grad(self.z)
         grad_z = grad_z.reshape(self.output_size, -1)
         self.grad_weights = np.dot(grad_z, self.input.T)
         self.grad_biases = grad_z.sum(axis=1, keepdims=True)
         grad_input = np.dot(self.weights.T, grad_z)
         return grad_input
+
+    # Activation functions and their gradients
+    @staticmethod
+    def relu(z):
+        return np.maximum(0, z)
+
+    @staticmethod
+    def relu_grad(z):
+        return (z > 0).astype(float)
+
+    @staticmethod
+    def sigmoid(z):
+        return 1 / (1 + np.exp(-z))
+
+    @staticmethod
+    def sigmoid_grad(z):
+        sig = 1 / (1 + np.exp(-z))
+        return sig * (1 - sig)
+
+    @staticmethod
+    def tanh(z):
+        return np.tanh(z)
+
+    @staticmethod
+    def tanh_grad(z):
+        return 1 - np.tanh(z) ** 2
+
+    @staticmethod
+    def linear(z):
+        return z
+
+    @staticmethod
+    def linear_grad(z):
+        return np.ones_like(z)
