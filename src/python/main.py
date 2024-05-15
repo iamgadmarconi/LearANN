dfdf42e2082de5e222ad54855bc204694714e430
Gad Marconi <gadmarconi4@outlook.com>
2024-05-15 13:20:29 +0200

    chore: Refactor CUDA and RNN code for improved performance and readability

--- src/python/main.py
+++ src/python/main.py
@@ -1,4 +1,4 @@
-from networks.rnn.tests.test_rnn import test_rnn, test_rnn_on_sine_wave, test_rnn_with_adagrad, test_rnn_with_adam, test_rnn_with_grad_descent, test_rnn_with_cuda
+from networks.rnn.tests.test_rnn import *
 
 
 if __name__ == '__main__':
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     # test_rnn_with_adam()
     # test_rnn_with_grad_descent()
 
-    test_rnn_with_cuda()
+    # test_rnn_with_cuda()
+    # test_rnn_with_cuda_on_sine_wave()
 
     print("Done!")
