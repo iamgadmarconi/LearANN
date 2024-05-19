332f158da2f45419ca44f8fd72c264d13dc89922
Gad Marconi <gadmarconi4@outlook.com>
2024-05-19 22:45:17 +0200

    Refactor gradient descent optimizer to use numba jit for improved performance

--- src/python/utils/optimizers/gradient_descent.py
+++ src/python/utils/optimizers/gradient_descent.py
@@ -1,5 +1,5 @@
 import numpy as np
-import numba
+from numba import jit
 
 from utils.optimizers.optimizers import Optimizer
 
@@ -8,7 +8,7 @@ class GradientDescent(Optimizer):
     def __init__(self, lr):
         super().__init__(lr)
 
-    @numba.njit
+    @jit(nopython=True, fastmath=True)
     def update(self, param, grad_param, name):
         param -= self.learning_rate * grad_param
         return param
